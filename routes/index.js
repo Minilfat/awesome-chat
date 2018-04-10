6d27a3f3282a3af62a64dd440dac153e17959d39
ZakShaker <zakan29@rambler.ru>
2018-04-10 20:23:57 +0300

    + added index page with registration + added profile page

--- routes/index.js
+++ routes/index.js
@@ -6,38 +6,41 @@ const {error404} = require('../controllers/errors');
 const authRequired = require('../middlewares/isAuthRequired');
 const logout = require('../controllers/logout');
 
-
-
-module.exports = (app) => {
-
+module.exports = app => {
     app.get('/', (req, res) => res.render('index', {message: req.flash('message')}));
-
 
     app.get('/login', (req, res) => res.render('index', {...res.locals}));
     app.post('/login', passport.authenticate('login', {
         successRedirect: '/chat',
         failureRedirect: '/',
-        failureFlash : true
+        failureFlash: true
     }));
-    app.get('/logout', (req, res) => logout(req,res));
+    app.get('/logout', (req, res) => logout(req, res));
 
     app.get('/register', (req, res) => res.render('register', {message: req.flash('message')}));
     app.post('/register', passport.authenticate('register', {
         successRedirect: '/chat',
         failureRedirect: '/register',
-        failureFlash : true 
+        failureFlash: true
     }));
 
     app.get('/contact-list', (req, res) => res.render('contact-list', {...res.locals}));
 
     app.get('/contact', (req, res) => res.render('contact', {...res.locals}));
 
-    // по этомму маршруту может пройти только авторизованный пользователь
+    // По этому маршруту может пройти только авторизованный пользователь
     app.get('/chat', authRequired(), (req, res) => {
-        res.render('chat', {username: req.user.getLogin(), 
-                            info: req.user.display()});
+        res.render('chat', {
+            username: req.user.getLogin(),
+            info: req.user.display()
+        });
     });
 
-
-
+    // По этомму маршруту может пройти только авторизованный пользователь
+    app.get('/profile', authRequired(), (req, res) => {
+        res.render('profile', {
+            username: req.user.getLogin(),
+            info: req.user.display()
+        });
+    });
 };
