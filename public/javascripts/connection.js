c025ec3dfb5576428f65eced1ab48c3b4da18a0f
Зуфар Махмутов <zufar.flash@yandex.ru>
2018-04-12 17:21:23 +0300

    socket connects now

--- public/javascripts/connection.js
+++ public/javascripts/connection.js
@@ -1,25 +1,26 @@
-import {addMessage} from 'addMessage';
+// import {addMessage} from 'addMessage';
 
-// Open connection
-export const connection = new WebSocket('ws://127.0.0.1:1337');
+// // Open connection
+// const connection = new WebSocket('ws://127.0.0.1:1337');
+// console.log(connection);
 
-connection.onopen = function () {
-    // First we want users to enter their names
-    console.log('Connection is opened');
-};
+// connection.onopen = function () {
+//     // First we want users to enter their names
+//     console.log('Connection is opened');
+// };
 
-connection.onerror = function (error) {
-    console.log('Error occured. Please, check connection with external server. Error:', error);
-};
+// connection.onerror = function (error) {
+//     console.log('Error occured. Please, check connection with external server. Error:', error);
+// };
 
-connection.onmessage = function (message) {
-    var json = {};
-    try {
-        json = JSON.parse(message.data);
-    } catch (e) {
-        console.log('Not a valid JSON ', message.data);
-    }
-    // Text, chatid, sender, date, type
-    addMessage(json.text, json.chatid, json.sender, json.date, json.type);
-}
+// connection.onmessage = function (message) {
+//     var json = {};
+//     try {
+//         json = JSON.parse(message.data);
+//     } catch (e) {
+//         console.log('Not a valid JSON ', message.data);
+//     }
+//     // Text, chatid, sender, date, type
+//     addMessage(json.text, json.chatid, json.sender, json.date, json.type);
+// }
 
